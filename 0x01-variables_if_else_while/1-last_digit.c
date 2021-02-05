f359a8250f641a1b9c83249e09a9ea356c1b6ea2
Monica Rojas <monicarojas1711@gmail.com>
2021-02-05 04:42:58 +0000

    Print alpahbets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ int main(void)
 	lastNumber = n % 10;
 
 	if (lastNumber == 0)
-		printf("Last digit of %i is %i and is 0\n", n, lastNumber);
+		printf("Last digit of %d is %d and is 0\n", n, lastNumber);
 	else if (lastNumber > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, lastNumber);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastNumber);
 	else
-		printf("Last digit of %i is %i and is less than 6 and not 0\n",
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 		       n, lastNumber);
 	return (0);
 }
