0d6f44636db693098839129a718c26db879ec2d8
Monica Rojas <monicarojas1711@gmail.com>
2021-02-05 21:42:39 +0000

    Print comb2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int Numb;
+
+	for (Numb = 0; Numb < 100; Numb++)
+	{
+		putchar(n / 10 + '0');
+		putchar(n % 10 + '0');
+	}
+
+	putchar('\n');
+	return (0);
+}
