2193b1b31117ad9184231b913c244cd2c32b9523
Monica Rojas <monicarojas1711@gmail.com>
2021-02-05 04:58:44 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	init ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar(ch + '0');
+	}
+	putchar("\n");
+	return (0);
+}
